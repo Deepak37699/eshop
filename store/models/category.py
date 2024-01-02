bffa4ce6e59238146e563d73b45fcb6cdbaa77fe
unknown <deepak.yadav97699@gmail.com>
2024-01-02 11:34:11 +0545

    auth and names in admin panels is shown

--- store/models/category.py
+++ store/models/category.py
@@ -1,3 +1,8 @@
-from django.db.models import models
+from django.db import models
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
